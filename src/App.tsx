7a9cc23cbeb723b565a65b95103c32c3851aef85
Alireza Dolati <alirezamacbook@Alirezas-MacBook-Air.local>
2024-11-29 15:15:06 +0330

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+import Massage from "./Message";
+function App() {
+return <div><Massage /></div>;
+}
+export default App;
---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+function Message() {
+return <h1>Abolfazl Key Berim (:</h1>;
+}
+export default Message;
+
